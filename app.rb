679b8ca03ffd276d8fe0227b70f0e04b42bb6cc5
saiindirat <tholusaiindira@gmail.com>
2025-05-19 22:07:33 +0000

    my seond commit

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
+require 'dotenv/load'
 require "sinatra"
 require "sinatra/reloader"
 require "http"
 
 get("/") do
 @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV['EXCHANGE_RATE_KEY']}")
+
+@string_response = @raw_response.to_s
+@parsed_response = JSON.parse(@string_response)
+@currencies = @parsed_response.fetch("currencies")
 erb(:homepage)
+end
+
+
+get("/:first_symbol") do
+  @the_symbol = params.fetch("first_symbol")
+  erb(:step_one)
 end
